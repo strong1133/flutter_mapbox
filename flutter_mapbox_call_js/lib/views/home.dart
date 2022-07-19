8f31a84568302ce9a20853a0f34dd8fa30738641
strong1133 <strong1133@naver.com>
2022-07-19 23:18:49 +0900

    location 추가

--- flutter_mapbox_call_js/lib/views/home.dart
+++ flutter_mapbox_call_js/lib/views/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_mapbox_call_js/widgets/mapbox_map.dart';
+import 'package:location/location.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,6 +13,11 @@ class Home extends StatelessWidget {
         child: Column(
       children: [
         Text('Home'),
+        SizedBox(
+          width: 400,
+          height: 400,
+          child: MapboxMap(),
+        ),
         SizedBox(
           width: 400,
           height: 400,
